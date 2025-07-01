0d651239fc43918afb7d2cb424e13dac8dc3e6f4
Ritik Ranjan <ritikranjan999@gmail.com>
2025-07-01 16:03:09 +0530

    Refactor BlogCard and Blogs components; update BlogCard to use content instead of description, integrate useAppContext in Blogs for filtering blogs based on input

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,16 +1,21 @@
 import { useNavigate } from "react-router";
 
+const IMAGE_URL_PREFIX = import.meta.env.VITE_CLOUDINARY_URL_PREFIX;
+
 const BlogCard = ({ blog }) => {
-    const { title, description, category, image, _id } = blog;
+    const { title, content, category, image, id } = blog;
     const navigate = useNavigate();
 
+    console.log(blog);
+
+
     return (
-        <div onClick={() => navigate(`/blog/${_id}`)} className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'>
-            <img src={image} className='aspect-video' />
-            <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full'>{category}</span>
+        <div onClick={() => navigate(`/blog/${id}`)} className='w-full rounded-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300 cursor-pointer'>
+            <img src={`${IMAGE_URL_PREFIX}/${image}`} className='aspect-video' />
+            <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full'>{category?.title}</span>
             <div className="p-5">
                 <h5 className="mb-2 font-medium text-foreground">{title}</h5>
-                <p className="mb-3 text-xs text-card-foreground" dangerouslySetInnerHTML={{ "__html": description.slice(0, 80) }}></p>
+                <p className="mb-3 text-xs text-card-foreground" dangerouslySetInnerHTML={{ "__html": content }}></p>
             </div>
         </div>
     )
--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -2,9 +2,20 @@ import { useState } from 'react'
 import { blog_data, blogCategories } from '../assets/assets'
 import { motion } from 'motion/react';
 import BlogCard from './BlogCard';
+import { useAppContext } from '../context/AppContext';
 
 const Blogs = () => {
     const [selectedCategory, setSelectedCategory] = useState('All');
+    const { blogs, input } = useAppContext();
+    const { posts } = blogs;
+
+
+    function filteredBlogs() {
+        if (input === '')
+            return posts;
+
+        return posts.filter((blog) => blog.title.toLowerCase().includes(input.toLowerCase()) || blog.category.toLowerCase().includes(input.toLowerCase()));
+    }
 
     return (
         <div>
@@ -26,9 +37,9 @@ const Blogs = () => {
             </div>
 
             <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-8 mb-24 mx-8 sm:mx-16 xl:mx-40 px-4 sm:px-8'>
-                {blog_data.filter((blog) => selectedCategory === 'All' || blog.category === selectedCategory)
+                {filteredBlogs().filter((blog) => selectedCategory === 'All' || blog.category === selectedCategory)
                     .map((blog) => (
-                        <BlogCard key={blog._id} blog={blog} />
+                        <BlogCard key={blog.id} blog={blog} />
                     ))}
             </div>
         </div>
